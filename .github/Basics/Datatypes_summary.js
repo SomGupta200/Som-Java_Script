fd95d530e84ce3340da45d1b1d89de5dc7532be9
Som Gupta <somgupta95894@gmail.com>
2025-12-20 10:18:13 +0000

    Knowledge about Datatypes ans their type

--- .github/Basics/Datatypes_summary.js
+++ .github/Basics/Datatypes_summary.js
@@ -4,5 +4,29 @@ console.log("som");
  // There are 7 types : string , Number , Boolean , Null
  // , Undefined , Symbol , BigInt .
 
+const id = Symbol('123')
+const anotherId = Symbol('123')
+console.log(id === anotherId);
+const bigNumber = 37439287745474389398n  // bigint number
+console.log(typeof bigNumber);
+
  // Reference (Non - Primitive)
  // Array , Objects , Functions
+
+// Array
+ const heroes = ["shaktiman" , "naagraj" , "Bhisma"]
+ //Object
+ let myObj = {  // object declare inside the Curly Braces
+    name:"som",
+    age: 20,
+    address:"Gwalior"
+ }
+ console.log(myObj);
+ // function
+ const myfunction=function(){
+    console.log("Hello World");
+ }
+console.log(typeof myfunction);
+
+ 
+ 
