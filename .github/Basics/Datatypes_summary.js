a1d931cae05e77ea4e767ad9cc97fd51be4004d8
Som Gupta <somgupta95894@gmail.com>
2025-12-20 09:44:26 +0000

    Add Datatypes_summary.js with basic datatype notes

---
+++ .github/Basics/Datatypes_summary.js
@@ -0,0 +1,8 @@
+console.log("som");
+
+// 1.Primitive
+ // There are 7 types : string , Number , Boolean , Null
+ // , Undefined , Symbol , BigInt .
+
+ // Reference (Non - Primitive)
+ // Array , Objects , Functions
