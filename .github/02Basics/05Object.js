76aaf136fe2c976da81171016ce605abdf3c0014
Som Gupta <somgupta95894@gmail.com>
2026-01-06 10:02:23 +0000

    Basic Array , Object

---
+++ .github/02Basics/05Object.js
@@ -0,0 +1,20 @@
+const course = {
+    conursename : "Physics",
+    price : "7999",
+    courseInstructor : "Som"
+}
+// console.log(course.courseInstructor);
+
+// Tradittional way
+//const money = course.price;
+
+
+// ------ Deconstructing ------ //
+//- Makes code shorter and cleaner.
+//- Helps avoid repetitive property access.
+//- Useful in function parameters when working with objects.
+
+// Deconstructing way 
+const {courseInstructor : instructor , price} = course
+console.log(instructor);
+
