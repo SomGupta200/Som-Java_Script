f7b8a4d4f8f37fc965206c79486565a52460067f
Som Gupta <somgupta95894@gmail.com>
2026-01-14 08:12:50 +0000

    Iterations

---
+++ .github/05_Iterations/two.js
@@ -0,0 +1,24 @@
+// While
+
+let index = 0;
+while(index <= 10){
+    console.log(`Value of index is ${index}`);
+    index = index + 2;
+}
+
+let myArray = ["Mahadev" , "Vishnu ji" , "Brahma dev"]
+let arr = 0
+while(arr < myArray.length){
+    console.log(`Pranam prabhu ${myArray[arr]}`);
+    arr = arr + 1;
+}
+
+// Do while
+// In do while loop for false condition the loop is 
+// execute atleast one time
+let score = 0
+do {
+    console.log(`The Score is ${score}`);
+    score++
+    
+} while (score <= 10);
